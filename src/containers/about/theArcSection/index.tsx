9a399bb1822b35536b86e28b7fb052d7aba11b2c
Praveen Yadav <praveenyadav7787@gmail.com>
2025-01-11 20:38:23 +0530

    update about section's layout as new mobile figma design

--- src/containers/about/theArcSection/index.tsx
+++ src/containers/about/theArcSection/index.tsx
@@ -5,7 +5,7 @@ export default function TheArcSection({ id, animate }: { id: string; animate: bo
     return (
         <div id={id} data-animate={animate} className={`${styles.container} relative flex items-center justify-end`}>
             <div className="flex flex-col">
-                <div className="heading">The Arch</div>
+                <div className="heading">The Arc</div>
                 <div className="subheading">Our architectural designs are a harmonious blend of form, function, and aesthetics, creating iconic spaces that stand the test of time. We believe that every building tells a story, and our expertise lies in crafting narratives through design.</div>
             </div>
             <img className={`${styles.curve} absolute-center`} src="/brand-curve.svg" alt="" />
