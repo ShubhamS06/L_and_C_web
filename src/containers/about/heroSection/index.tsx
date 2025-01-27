eea5c9a286b73efa4c1321647a7d7ae3a233c930
Praveen Yadav <praveenyadav7787@gmail.com>
2025-01-28 00:06:13 +0530

    UI change from 2 to 9

--- src/containers/about/heroSection/index.tsx
+++ src/containers/about/heroSection/index.tsx
@@ -4,7 +4,7 @@ export default function HeroSection() {
 
     return (
         <div className={`${styles.container} relative flex flex-col items-start justify-end`}>
-            <div className={`${styles.heading} heading w-fit`}>Lines N Curves</div>
+            <div className={`${styles.heading} heading w-fit m-0`}>Lines N Curves</div>
             <div className={`${styles.subHeading} subheading`}>Lines N Curves is a design atelier that seamlessly integrates architecture, landscape, and interior design. We craft spaces that are not just aesthetically pleasing but also functional and sustainable, reflecting the unique essence </div>
         </div>
     )
