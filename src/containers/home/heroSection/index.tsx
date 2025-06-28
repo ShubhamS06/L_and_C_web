9725de3ac8a8d064944e18fa505fe53b2ea0c4f2
Shubham Sahu <shubham.sahu@techstalwarts.com>
2025-06-28 15:43:26 +0530

    home hero text animation delay changed

--- src/containers/home/heroSection/index.tsx
+++ src/containers/home/heroSection/index.tsx
@@ -5,7 +5,8 @@ export default function HeroSection() {
 
     return (
         <div className={`${styles.container} relative flex flex-col items-center justify-center`}>
-            <div className={styles.title}>{heroSectionData.title}</div>
+            <div className={styles.title_t}>{heroSectionData.title}</div>
+            {/* <div className={styles.title}>{heroSectionData.title}</div> */}
             <div className={styles.heading}>{heroSectionData.heading}</div>
             <div className={styles.subHeading}>{heroSectionData.subHeading}</div>
         </div>
